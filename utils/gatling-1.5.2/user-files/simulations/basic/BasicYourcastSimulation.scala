5b844c5b7a2e4d8f57573edd0d2b5cc52e190da9
Fabien Foerster <fabienfoerster@gmail.com>
2013-08-02 11:06:04 +0200

    add util gatling

---
+++ utils/gatling-1.5.2/user-files/simulations/basic/BasicYourcastSimulation.scala
@@ -0,0 +1,19 @@
+package basic
+
+import com.excilys.ebi.gatling.core.Predef._
+import com.excilys.ebi.gatling.http.Predef._
+import com.excilys.ebi.gatling.jdbc.Predef._
+import com.excilys.ebi.gatling.http.Headers.Names._
+import akka.util.duration._
+import bootstrap._
+
+class BasicYourcastSimulation extends Simulation {
+  // your code starts here
+  val scn = scenario("My Very First Gatling Scenario")
+            .exec(http("Yourcast Client")
+              .get("http://erebe-vm1.unice.fr:8080")) 
+  
+  setUp(scn.users(10))
+  // your code ends here
+}
+
