13d165f7cc239fe982417c3a80508e044cfa1a87
Fabien Foerster <fabienfoerster@gmail.com>
2013-08-13 15:57:24 +0200

    fix align same date data with large date range

--- extractor/src/main/java/yourcast/mongodb/extractor/CollectdDataWritorMonitoring.java
+++ extractor/src/main/java/yourcast/mongodb/extractor/CollectdDataWritorMonitoring.java
@@ -10,9 +10,7 @@ import org.apache.poi.ss.usermodel.Sheet;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -49,42 +47,47 @@ public class CollectdDataWritorMonitoring extends CollectdDataWritor {
             createColumnName(entry.getValue().copy(),entry.getKey());
         }
         lastColumToWrite = 1 ;
-        int number_values = 0 ;
+        int number_values;
         int i = 1 ;
         boolean keepOnLooping = true ;
+        DBObject[] oldData = new DBObject[cursors.size()];
         while(keepOnLooping){
             keepOnLooping = false ;
+            int k = 0 ;
             for(Map.Entry<String,DBCursor> entry : cursors.entrySet()){
+                number_values = 0 ;
                 keepOnLooping = keepOnLooping || entry.getValue().hasNext() ;
                 if(entry.getValue().hasNext()){
-                    data = entry.getValue().next();
+                    if(oldData[k] == null){
+                        data = entry.getValue().next();
+                        oldData[k] = data ;
+                    } else {
+                        data = oldData[k];
+                    }
                     cal.setTime((Date)data.get("time"));
                     String date = simpleDateFormat.format(cal.getTime());
                     r = getRow(sheet,i);
                     c = getCell(r,0);
-                    int k = i ;
-                    while(!c.getStringCellValue().isEmpty()&& !c.getStringCellValue().equals(date)){
-                        k++;
-                        r = getRow(sheet,k);
-                        c = getCell(r,0);
-                    }
-
-                    c.setCellValue(date);
-                    BasicDBList values = (BasicDBList) data.get("values");
-                    number_values = values.size() ;
-                    for(int j = 0 ; j < values.size() ; j++){
-                        c = getCell(r , lastColumToWrite +j);
-                        double value =  (Double)values.get(j);
-                        c.setCellValue(value);
-
+                    if(c.getStringCellValue().isEmpty() || date.equals(c.getStringCellValue())){
+                        c.setCellValue(date);
+                        BasicDBList values = (BasicDBList) data.get("values");
+                        number_values = values.size() ;
+                        for(int j = 0 ; j < number_values ; j++){
+                            c = getCell(r , lastColumToWrite +j);
+                            double value =  (Double)values.get(j);
+                            c.setCellValue(value);
+                        }
+                        oldData[k] = null ;
                     }
                 }
+                k++;
                 lastColumToWrite += number_values ;
             }
             i++;
             lastColum = lastColumToWrite > lastColum ? lastColumToWrite : lastColum ;
             lastColumToWrite = 1 ;
         }
+        setAutoSizeColum();
     }
 
 
@@ -120,7 +123,6 @@ public class CollectdDataWritorMonitoring extends CollectdDataWritor {
 
     @Override
     public void close() throws IOException {
-        setAutoSizeColum();
         super.close();
         lastColumToWrite = 1 ;
     }
