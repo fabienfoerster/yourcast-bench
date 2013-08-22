4943ad0198eb22d598898dfcd758514a5f521a1a
Fabien Foerster <fabienfoerster@gmail.com>
2013-08-22 10:32:03 +0200

    scenario write start and end time to file to know the exact duration of the simulation

--- src/test/scala/BasicYourcastSimulation.scala
+++ src/test/scala/BasicYourcastSimulation.scala
@@ -1,5 +1,6 @@
 
-
+import java.util.Calendar
+import java.io.PrintWriter
 import com.excilys.ebi.gatling.core.Predef._
 import com.excilys.ebi.gatling.http.Predef._
 import com.excilys.ebi.gatling.jdbc.Predef._
@@ -8,14 +9,35 @@ import akka.util.duration._
 import bootstrap._
 
 class BasicYourcastSimulation extends Simulation {
-  // your code starts here
-  val scn = scenario("My Very First Gatling Scenario")
-            .exec(http("Yourcast Client")
+  val start = Calendar.getInstance().getTime().getTime()
+  val start_writer = new PrintWriter("/tmp/start_experiment","UTF-8")
+  start_writer.println(start)
+  start_writer.close()
+  val scn = scenario("My Very First Gatling Scenario").exec(session => {
+				  val start = Calendar.getInstance().getTime().getTime()
+				  val start_writer = new PrintWriter("/tmp/start_experiment","UTF-8")
+				  start_writer.println(start)
+				  start_writer.close()
+				  session
+  			})
+            .repeat(50) {
+                exec(
+                http("Yourcast Client")
               .get("http://erebe-vm1.unice.fr:8080"))
-            .pause(5)
-              
+            .pause(1)
+            }.exec(session => {
+				  val end = Calendar.getInstance().getTime().getTime()
+				  val end_writer = new PrintWriter("/tmp/end_experiment","UTF-8")
+				  end_writer.println(end)
+				  end_writer.close()
+				  session
+            })
   
   setUp(scn.users(10))
+  val end = Calendar.getInstance().getTime().getTime()
+  val end_writer = new PrintWriter("/tmp/end_experiment","UTF-8")
+  end_writer.println(end)
+  end_writer.close()
   // your code ends here
 }
 
